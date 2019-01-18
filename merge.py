c9ab3e74d20a1f3f39fe959d3d1695daa6fdc5da
Dor Arad <dorarad@stanford.edu>
2019-01-17 18:50:09 -0800

    fixes for merge script

--- merge.py
+++ merge.py
@@ -1,37 +1,48 @@
 # Script to merge hdf5 chunk files to one and update info.json accordingly
 
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+warnings.filterwarnings("ignore", message="size changed")
+
 from tqdm import tqdm
+import argparse
 import h5py 
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type = str, help = "features directory name")
+parser.add_argument('--chunksNum', type = int, default = 11, help = "number of file chunks")
+parser.add_argument('--chunkSize', type = int, default = 10000, help = "file chunk size")
 args = parser.parse_args()
+
+print("Merging features file for gqa_{}. This may take a while.".format(args.name))
 
 # Format specification for features files
 spec = {
-	"spatial": {"features": (108079, 2048, 7, 7)}
+	"spatial": {"features": (108079, 2048, 7, 7)},
 	"objects": {"features": (108077, 100, 2048),
-							(108077, 100, 4)}
+				"bboxes": (108077, 100, 4)}
 }
-chunkSize = 10000
 
 # Merge hdf5 files
-with h5py.File("data/{name}/gqa_{name}.h5".format(name = args.name)) as out:
+with h5py.File("data/gqa_{name}.h5".format(name = args.name)) as out:
 	datasets  = {}
 	for dname in spec[args.name]:
 		datasets[dname] = out.create_dataset(dname, spec[args.name][dname])
 
-	for i in tqdm(range(11)):
+	for i in tqdm(range(args.chunksNum)):
 		with h5py.File("data/{name}/gqa_{name}_{index}.h5".format(name = args.name, index = i)) as chunk: 
 			for dname in spec[args.name]:
-				datasets[dname][(i * chunkSize):((i + 1) * chunkSize)] = chunk[dname][:]
+				low = i * args.chunkSize
+				high = (i + 1) * args.chunkSize if i < args.chunksNum -1 else spec[args.name][dname][0]
+				datasets[dname][low:high] = chunk[dname][:]
 
 # Update info file
 with open("data/{name}/gqa_{name}_info.json".format(name = args.name)) as infoIn:
 	info = json.load(infoIn)
 	for imageId in info:
-		info[imageId]["index"] = info[imageId]["file"] * chunkSize + info[imageId]["idx"]
+		info[imageId]["index"] = info[imageId]["file"] * args.chunkSize + info[imageId]["idx"]
 		del info[imageId]["file"]
 
-	with open("data/{name}/gqa_{name}_merged_info.json".format(name = args.name)) as infoOut:
-		json.dump(info, infoMerged)
+	with open("data/gqa_{name}_merged_info.json".format(name = args.name), "w") as infoOut:
+		json.dump(info, infoOut)
