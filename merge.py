598ebfb228fc309809f5bb11beb9668f91bbc54d
Drew Arad Hudson <dorarad@stanford.edu>
2019-03-29 12:59:46 -0700

    update to continue test features

--- merge.py
+++ merge.py
@@ -11,7 +11,7 @@ import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--name', type = str, help = "features directory name")
-parser.add_argument('--chunksNum', type = int, default = 11, help = "number of file chunks")
+parser.add_argument('--chunksNum', type = int, default = 16, help = "number of file chunks")
 parser.add_argument('--chunkSize', type = int, default = 10000, help = "file chunk size")
 args = parser.parse_args()
 
@@ -19,12 +19,13 @@ print("Merging features file for gqa_{}. This may take a while.".format(args.nam
 
 # Format specification for features files
 spec = {
-	"spatial": {"features": (108079, 2048, 7, 7)},
-	"objects": {"features": (108077, 100, 2048),
-				"bboxes": (108077, 100, 4)}
+	"spatial": {"features": (148690, 2048, 7, 7)},
+	"objects": {"features": (148690, 100, 2048),
+				"bboxes": (148690, 100, 4)}
 }
 
 # Merge hdf5 files
+lengths = []
 with h5py.File("data/gqa_{name}.h5".format(name = args.name)) as out:
 	datasets  = {}
 	for dname in spec[args.name]:
@@ -32,16 +33,21 @@ with h5py.File("data/gqa_{name}.h5".format(name = args.name)) as out:
 
 	for i in tqdm(range(args.chunksNum)):
 		with h5py.File("data/{name}/gqa_{name}_{index}.h5".format(name = args.name, index = i)) as chunk: 
+			low = 0
 			for dname in spec[args.name]:
-				low = i * args.chunkSize
-				high = (i + 1) * args.chunkSize if i < args.chunksNum -1 else spec[args.name][dname][0]
+				high = low + chunk[dname].shape[0]
+				#low = i * args.chunkSize
+				#high = (i + 1) * args.chunkSize if i < args.chunksNum -1 else spec[args.name][dname][0]
 				datasets[dname][low:high] = chunk[dname][:]
+				low = high
+				lengths.append(high)
 
 # Update info file
 with open("data/{name}/gqa_{name}_info.json".format(name = args.name)) as infoIn:
 	info = json.load(infoIn)
 	for imageId in info:
-		info[imageId]["index"] = info[imageId]["file"] * args.chunkSize + info[imageId]["idx"]
+		info[imageId]["index"] = lengths[info[imageId]["file"]] + info[imageId]["idx"] 
+		# info[imageId]["index"] = info[imageId]["file"] * args.chunkSize + info[imageId]["idx"]
 		del info[imageId]["idx"]
 		del info[imageId]["file"]
 
